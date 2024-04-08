a585eb89e8933d6591942a626d523f072f8fb71e
thuanguyen <thuanguyen10865@gmail.com>
2024-04-08 00:03:19 -0400

    tn

--- protein_ligand/colabfold/test/test2_hpc.sh
+++ protein_ligand/colabfold/test/test2_hpc.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-#BSUB -n 4
-#BSUB -J tes_gpu
-#BSUB -W 1:00
-#BSUB -q gpu
-#BSUB -gpu "num=1:mode=shared:mps=no"
+# LSF job submission: 1 core, 30 min runtime, 16GB memory, 1 shared GPU with MPS on the new_gpu queue
+#BSUB -n 1                          # Core count
+#BSUB -J test_gpu                   # Job name
+#BSUB -W 30                         # Runtime (minutes)
+#BSUB -R "rusage[mem=16GB]"         # Memory request
+#BSUB -q gpu                        # Queue selection
+#BSUB -gpu "num=1:mode=shared:mps=yes" # GPU request
+#BSUB -o out.%J                     # Output file
+#BSUB -e err.%J                     # Error file                  
 
-# Load the CUDA module
-module load cuda
+# Source the bash profile to ensure conda and other environment variables are available
+source .bashrc
 
-# Execute Jupyter notebook
-jupyter nbconvert --to notebook --execute --inplace /home6/tvnguye4/google_cloud/protein_ligand/colabfold/test/test2.ipynb
+# Activate the Conda environment
+conda activate /usr/local/usrapps/probioticengring/tvnguyen/conda/envs/tvnEnv0003_colabfold
+
+# Set CUDA 12.0 as the CUDA environment
+export PATH=/usr/local/apps/cuda/cuda-12.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/apps/cuda/cuda-12.0/lib64:$LD_LIBRARY_PATH
+export CUDA_HOME=/usr/local/apps/cuda/cuda-12.0
+
+# Verify CUDA Compiler Version to ensure CUDA 12.0 is loaded properly
+nvcc -V
+
+# Check NVIDIA System Management Interface to confirm GPU allocation
+nvidia-smi
+
+# Execute the Jupyter notebook, converting it in place to ensure all cells are executed
+jupyter nbconvert --to notebook --execute --inplace /share/probioticengring/tvnguye4/google_cloud/protein_ligand/colabfold/test/test2.ipynb
+
+# Deactivate the Conda environment
+conda deactivate
